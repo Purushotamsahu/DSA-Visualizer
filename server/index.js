0f11183ea08d594e2dd5c60d88509d1358fe36bb
Purushotam sahu <purushotam176@gmail.com>
2026-03-16 22:33:03 +0530

    Fix: Update CORS policy to allow Vercel origins

--- server/index.js
+++ server/index.js
@@ -39,7 +39,11 @@ const User = require('./models/User');
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 
 app.use((req, res, next) => {
